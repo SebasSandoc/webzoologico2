2d14e4f037ce8cfea61c1363436d6bbf533fc190
Davidparra <df.parra.07@gmail.com>
2024-10-30 17:05:45 -0500

    PUT, POST, DELETE

--- src/app/components/animal/animal.component.ts
+++ src/app/components/animal/animal.component.ts
@@ -3,6 +3,8 @@ import { AnimalService } from '../../services/animal.service';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-animal',
@@ -19,35 +21,116 @@ export class AnimalComponent {
   constructor(private animalService: AnimalService,
     private formBuilder: FormBuilder,
     private router: Router,
-    private toastr: ToastrService) { 
-      this.animalForm= this.formBuilder.group({
-        nombre: '',
-        edad: 0,
-        tipo: '',
-        fecha: Date
-      })
-    }
+    private toastr: ToastrService) {
+    this.animalForm = this.formBuilder.group({
+      nombre: '',
+      edad: 0,
+      tipo: '',
+      fecha: Date
+    })
+  }
+  newMessage(messageText: string) {
+    this.toastr.success('Clic aquí para actualizar la lista', messageText)
+      .onTap
+      .pipe(take(1))
+      .subscribe(() => window.location.reload());
+  }
+
   getAllAnimals() {
     this.animalService.getAllAnimalsData().subscribe((data: {}) => {
       this.animalList = data;
     });
   }
   newAnimalEntry() {
-    
-    this.animalService.newAnimal( this.animalForm.value).subscribe(
+
+    this.animalService.newAnimal(this.animalForm.value).subscribe(
       () => {
         //Redirigiendo a la ruta actual /animal y recargando la ventana
         this.router.navigate(['/animal']).then(() => {
-          //this.newMessage('Registro exitoso');
+          this.newMessage('Registro exitoso');
         })
       }
     );
   }
 
+  updateAnimalEntry() {
+    //Removiendo valores vacios del formulario de actualización
+    for (let key in this.animalForm.value) {
+      if (this.animalForm.value[key] === '') {
+        this.animalForm.removeControl(key);
+      }
+    }
+    this.animalService.updateAnimal(this.idAnimal, this.animalForm.value).subscribe(
+      () => {
+        //Enviando mensaje de confirmación
+        this.newMessage("Animal editado");
+      }
+    );
+  }
+
+  deleteAnimalEntry(id: any) {
+    console.log(id)
+    this.animalService.deleteAnimal(id).subscribe(
+      () => {
+        //Enviando mensaje de confirmación
+        this.newMessage("Animal eliminado");
+      }
+    );
+  }
 
   ngOnInit() {
     this.getAllAnimals();
   }
+
+  getValidDate(fecha: Date) {
+    const fechaFinal: Date = new Date(fecha);
+    //separado los datos
+    var dd = fechaFinal.getDate() + 1;//fue necesario porque siempre daba un día antes
+    var mm = fechaFinal.getMonth() + 1; //porque Enero es 0
+    var yyyy = fechaFinal.getFullYear();
+    var mes = '';
+    var dia = '';
+    //Como algunos meses tienen 31 días dd puede dar 32
+    if (dd == 32) {
+      dd = 1;
+      mm++;
+    }
+    //Transformación de fecha cuando el día o mes son menores a 10
+    //se le coloca un cero al inicio
+    //Día
+    if (dd < 10) {
+      dia += `0${dd}`;
+    } else {
+      dia += `${dd}`;
+    }
+    //Mes
+    if (mm < 10) {
+      mes += `0${mm}`;
+    } else {
+      mes += `${mm}`;
+    }
+    //formatDate para colocar la fecha en un formato aceptado por el calendario
+    //GMT-0500 es para Colombia
+    var finalDate = formatDate(new Date(yyyy + '-' + mes + '-' + dia + ' GMT-0500'), 'yyyy-MM-dd', "en-US");
+    return finalDate;
+  }
+
+  toggleEditAnimal(id: any) {
+    this.idAnimal = id;
+    console.log(this.idAnimal)
+    this.animalService.getOneAnimal(id).subscribe(
+      data => {
+        this.animalForm.setValue({
+          nombre: data.nombre,
+          edad: data.edad,
+          tipo: data.tipo,
+          fecha: this.getValidDate(data.fecha)
+        });
+      }
+    );
+    this.editableAnimal = !this.editableAnimal;
+  }
+
 }
 
 
